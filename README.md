1db0f4c1077e32cb751033b9c2883eece06c6186
Esteban Volentini <evolentini@equiser.com.ar>
2020-03-01 20:48:43 -0300

    Archivos iniciales para la primera parte

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ids-tp1
+# Trabajo Practico Numero 1
+
 Repositorio inicial para el Trabajo Practico 1 de la asignatura de Ingenieria de Software
+
+## Primera Parte
+
+1. Clonar este repositorio en su computadora y desplegar la rama *master*.
+
+2. Agregar al archivo alumnos una funcion que serialice sus datos personales similar a la del siguiente ejemplo. La declaración y la definición de la funcion debe agregarse abajo de las existentes.
+
+    ```c
+    bool EstebanVolentini(char * cadena, size_t espacio) {
+        struct alumno_s alumno = {
+            .apellidos = "VOLENTINI",
+            .nombres = "Esteban Daniel",
+            .documento = "23.517.968",
+        };
+
+        SerializarAlumno(cadena, sizeof(cadena), &alumno);
+    }
+    ```
+
+3. Confirmar los cambios, resolver los conflictos y subir los cambios al servidor.
---
+++ src/alumnos.c
@@ -0,0 +1,49 @@
+/*=====[Module Name]===========================================================
+ * Copyright 2019 Esteban Daniel VOLENTINI <evolentini@gmail.com>
+ * All rights reserved.
+ * License: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.1.0
+ * Creation Date: 2019/03/01
+ */
+ 
+/*=====[Inclusion of own header]=============================================*/
+
+#include "alumnos.h"
+#include <stdio.h>
+
+/*=====[Inclusions of private function dependencies]=========================*/
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Private function-like macros]========================================*/
+
+/*=====[Definitions of private data types]===================================*/
+
+/*=====[Definitions of external public global variables]=====================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Prototypes (declarations) of private functions]======================*/
+
+/*=====[Implementations of public functions]=================================*/
+
+/*=====[Implementations of interrupt functions]==============================*/
+
+bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
+    int resultado;
+    const char FORMATO[] = "{"
+        "\"documento\":\"%s\","
+        "\"apellidos\":\"%s\","
+        "\"nombres\":\"%s\""
+    "}";
+
+    resultado = snprintf(cadena, espacio, FORMATO, 
+             alumno->documento, alumno->apellidos, alumno->nombres);
+
+    return (resultado >= 0);
+}
+
+/*=====[Implementations of private functions]================================*/
